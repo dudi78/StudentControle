c1c2153646ed6696071e0bba3d1c82db780b04bc
dunia <hinddouniati@gmail.com>
2024-12-18 17:16:01 +0100

    Final Commit

--- src/main/java/ma/xproce/controle/Web/StudentController.java
+++ src/main/java/ma/xproce/controle/Web/StudentController.java
@@ -17,8 +17,8 @@ public class StudentController {
     StudentService studentService;
 
     @QueryMapping
-    public StudentDto getStudentByDateNaissance(@Argument Date datenaiss){
-        return studentService.GetStudentByDatenaissance(datenaiss);
+    public StudentDto getStudentByDateNaissance(@Argument Date dateNaiss){
+        return studentService.GetStudentByDatenaissance(dateNaiss);
     }
     @MutationMapping
     public StudentDto saveStudent(@Argument StudentDto studentDto){
